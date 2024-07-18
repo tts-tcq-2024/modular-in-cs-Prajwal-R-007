105dd0801340323b5a13876f30d69812540f507f
Prajwal-R-007 <R.Prajwal2@in.bosch.com>
2024-07-18 16:58:21 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,52 +1,9 @@
-using System;
-using System.Diagnostics;
-using System.Drawing;
-using ColorFromPairNumber;
-using PairNumberFromColor;
+using TelCo.ColorCoder;
 
-namespace TelCo.ColorCoder
+internal class Program
 {
-    class Program
+    private static void Main()
     {
-        private static void Main(string[] args)
-        {
-            int pairNumber = 4;
-            ColorPair testPair1 = ColorFromPairNumber.ColorFromPairNumber.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.Brown);
-
-            pairNumber = 5;
-            testPair1 = ColorFromPairNumber.ColorFromPairNumber.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.White);
-            Debug.Assert(testPair1.minorColor == Color.SlateGray);
-
-            pairNumber = 23;
-            testPair1 = ColorFromPairNumber.ColorFromPairNumber.GetColorFromPairNumber(pairNumber);
-            Console.WriteLine("[In]Pair Number: {0},[Out] Colors: {1}\n", pairNumber, testPair1);
-            Debug.Assert(testPair1.majorColor == Color.Violet);
-            Debug.Assert(testPair1.minorColor == Color.Green);
-
-            ColorPair testPair2 = new() { majorColor = Color.Yellow, minorColor = Color.Green };
-            pairNumber = PairNumberFromColor.PairNumberFromColor.GetPairNumberFromColor(testPair2);
-            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}\n", testPair2, pairNumber);
-            Debug.Assert(pairNumber == 18);
-
-            testPair2 = new ColorPair() { majorColor = Color.Red, minorColor = Color.Blue };
-            pairNumber = PairNumberFromColor.PairNumberFromColor.GetPairNumberFromColor(testPair2);
-            Console.WriteLine("[In]Colors: {0}, [Out] PairNumber: {1}", testPair2, pairNumber);
-            Debug.Assert(pairNumber == 6);
-        }
-
-        internal class ColorPair
-        {
-            internal Color majorColor;
-            internal Color minorColor;
-            public override string ToString()
-            {
-                return string.Format("MajorColor:{0}, MinorColor:{1}", majorColor.Name, minorColor.Name);
-            }
-        }
+        ColorToPairNumberViceVersaTests.RunTests();
     }
 }
